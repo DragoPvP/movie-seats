bb7e3e9b221a6fff078af409c6551ad11a54ceeb
unknown <heshamkhan20@gmail.com>
2021-05-11 04:06:59 +0530

    JavaScript functions done added Calc and selectors

--- script.js
+++ script.js
@@ -4,13 +4,24 @@ const count = document.getElementById('count');
 const total = document.getElementById('total');
 const movieSelect = document.getElementById('movie');
 
-const ticketPrice = +movieSelect.value;
+let ticketPrice = +movieSelect.value;
 
 //Update total and count
 function updatedSelectedCount() {
-    
+    const selectedSeats = document.querySelectorAll('.row .seat.selected');
+    const selectedSeatsCount = selectedSeats.length;
+
+    count.innerText = selectedSeatsCount;
+    total.innerText = selectedSeatsCount * ticketPrice;
 }
 
+//Movie Select Event
+movieSelect.addEventListener('change' , e => {
+    ticketPrice = e.target.value;
+    updatedSelectedCount();
+});
+
+//Seat click event
 container.addEventListener('click', function(e) {
     if (e.target.classList.contains('seat') && !e.target.classList.contains('occupied')) {
         e.target.classList.toggle('selected');
